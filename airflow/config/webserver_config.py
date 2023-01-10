2129844c79d16258ec3d1096f3a358ba6694b9bc
jered.little <jeredlittle1996@gmail.com>
2023-01-10 15:02:47 -0700

    add csrf

--- airflow/config/webserver_config.py
+++ airflow/config/webserver_config.py
@@ -2,4 +2,5 @@ import os
 from airflow import configuration as conf
 basedir = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_DATABASE_URI = conf.get('core', 'SQL_ALCHEMY_CONN')
+CSRF_ENABLED = True
 AUTH_ROLE_PUBLIC = 'User'
